d2b4fddb4bc31ec35b040a9386ed7e73545f75de
Felipe Paz <xxfelipepaz@gmail.com>
2025-06-07 05:12:00 -0300

    refactor: update domain entries in insert.sql and add cleanup commands

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,11 +1,9 @@
 -- SQLite
-INSERT INTO domains(id, name, created_at) VALUES(1, 'google.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(2, 'facebook.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(3, 'twitter.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(4, 'instagram.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(5, 'youtube.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(6, 'linkedin.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(7, 'github.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(8, 'stackoverflow.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(9, 'reddit.com', '2021-01-01');
-INSERT INTO domains(id, name, created_at) VALUES(10, 'pinterest.com', '2021-01-01');
+INSERT INTO domains(id, name, created_at) VALUES(1, 'meudominio.com', '2021-01-01');
+INSERT INTO domains(id, name, created_at) VALUES(2, 'whitelabel.com', '2021-01-01');
+INSERT INTO domains(id, name, created_at) VALUES(3, 'tiopatinhas.com', '2021-01-01');
+
+
+DELETE from domains;
+
+select * from domains;
